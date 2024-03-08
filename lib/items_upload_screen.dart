2e2f91d494815a336192d94d319bb5c59e6c7add
Lucas Nogueira <nozescrypto@gmail.com>
2024-03-07 22:22:49 -0300

    create page upload

--- lib/items_upload_screen.dart
+++ lib/items_upload_screen.dart
@@ -190,8 +190,46 @@ class _ItemsUploadScreenState extends State<ItemsUploadScreen> {
     );
   }
 
+//default Screen
+
+  Widget defaultScreen() {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.purple,
+            title: const Text(
+              'Upload New Item',
+              style: TextStyle(
+                  fontSize: 18,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            )),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.add_photo_alternate,
+                color: Colors.white,
+                size: 200,
+              ),
+              ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black54),
+                  onPressed: () {},
+                  child: const Text(
+                    'Add New Item',
+                    style: TextStyle(color: Colors.white70),
+                  ))
+            ],
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return uploadFormScreen();
+    return defaultScreen();
   }
 }
